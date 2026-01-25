00eb71074c4b84d5bfbf619c89c0cb65b82c1e35
Md Imran Shakeel <imshkl2005@gmail.com>
2026-01-25 11:33:27 +0530

    Update duplicatearr47.cpp

--- duplicatearr47.cpp
+++ duplicatearr47.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 using namespace std;
-int dupli(int arr[],int n){
-    int count=0;
-    for(int i=0;i<n;i++){
-    if(arr[2]=arr[i]){
-        count++;
+int dupli(int arr[], int n, int key){
+    int count = 0;
+
+    for(int i = 0; i < n; i++){
+        if(arr[i] == key){   // comparison
+            count++;
+        }
     }
-    }return count;
+
+    return count;
+
 }
 int  main(){
     int arr[5]={1,2,3,4,3};
