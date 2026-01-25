cb7d2ab56c37faf5d21aca4315ff94f91115a319
Md Imran Shakeel <imshkl2005@gmail.com>
2026-01-25 15:51:33 +0530

    Improve getpivot function readability and comments
    Refactor getpivot function with added comments for clarity.

--- pivotinarr54.cpp
+++ pivotinarr54.cpp
@@ -1,24 +1,40 @@
 #include<iostream>
 using namespace std;
-int getpivot(int arr[],int n){
-    int s=0;
-    int e=n-1;
-    int mid=s+(e-s)/2;
-    while(s<e){
-        if( arr[mid]>=arr[0]){
-            s=mid+1;
+int getpivot(int arr[], int n){
+
+    int s = 0;          // start
+    int e = n - 1;      // end
+
+    int mid = s + (e - s) / 2;
+
+    while(s < e){
+
+        // If mid element is >= first element,
+        // then we are in LEFT sorted part
+        // so pivot must be on RIGHT side
+        if(arr[mid] >= arr[0]){
+            s = mid + 1;
         }
+
+        // Otherwise we are in RIGHT part
+        // pivot lies on LEFT side (including mid)
         else{
-            e=mid;
+            e = mid;
         }
-        mid=s+(e-s)/2;
+
+        // recalculate mid
+        mid = s + (e - s) / 2;
     }
-    return s;//return e; will also give same
+
+    // when s == e, that index is pivot
+    return s;   // return e also works because s == e
 }
+
 int main(){
     int arr[5]={8,10,17,1,3};
     cout<<"Pivot is "<<getpivot(arr,5);
     return 0;
 }
 //OUTPUT-
+
 //Pivot is 3
