9f71dccabbb1fd441b4e4a1fd7c069ccda6aff4a
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-02 20:22:20 +0530

    Refactor pair sum algorithm in pairsumarr49.cpp
    Refactored pair sum logic to avoid duplicate pairs and improved readability.

--- pairsumarr49.cpp
+++ pairsumarr49.cpp
@@ -1,19 +1,22 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int x=5;
-    int arr[5]={1,2,3,4,6};
-    for(int i=0;i<5;i++){
-     int ele1=arr[i];{
-        for(int j=0;j<5;j++){
-            int ele2=arr[j];
-          if(arr[j]>arr[i]){  if(ele1+ele2==5){
-                cout<<arr[i]<<""<<arr[j]<<"  ";
-            }}
+
+int main() {
+
+    int x = 5;
+    int arr[] = {1,2,3,4,6};
+
+    for(int i = 0; i < 5; i++) {
+
+        for(int j = i + 1; j < 5; j++) {
+
+            if(arr[i] + arr[j] == x) {
+
+                cout << arr[i] << " " << arr[j] << endl;
+
+            }
         }
-     }
     }
+
     return 0;
 }
-//OUTPUT-
-//32 41
