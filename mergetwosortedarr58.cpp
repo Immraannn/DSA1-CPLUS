b693fe532ddd04e85ebae6b2e02c0f03ec26ca5d
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-02 21:54:51 +0530

    Implement merging of two sorted arrays

---
+++ mergetwosortedarr58.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int arr1[]={1,3,5};
+    int arr2[]={2,4,6};
+
+    int n1=3,n2=3;
+
+    int i=0,j=0;
+
+    while(i<n1 && j<n2){
+
+        if(arr1[i]<arr2[j]){
+            cout<<arr1[i]<<" ";
+            i++;
+        }
+
+        else{
+            cout<<arr2[j]<<" ";
+            j++;
+        }
+    }
+
+    while(i<n1){
+        cout<<arr1[i]<<" ";
+        i++;
+    }
+
+    while(j<n2){
+        cout<<arr2[j]<<" ";
+        j++;
+    }
+}
