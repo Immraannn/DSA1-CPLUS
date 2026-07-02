fd238f2d9db015f4087e58cc199896bdd5fd46ab
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-02 20:24:16 +0530

    Refactor triplet sum algorithm for clarity and efficiency
    Refactored the triplet sum algorithm to improve readability and efficiency. Updated the logic to avoid unnecessary iterations and added a target variable for clarity.

--- tripletsumarr50.cpp
+++ tripletsumarr50.cpp
@@ -1,21 +1,29 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int x=5;
-    int arr[6]={4,5,3,6,0,6};
-    for(int i=0;i<6;i++){
-     int ele1=arr[i];{
-        for(int j=0;j<6;j++){
-            int ele2=arr[j];
-          for(int k=0;k<6;k++){
-            int ele3=arr[k];
-            if(ele1+ele2+ele3==12 &&i>j &&j>k){
-                cout<<"("<<ele1<<","<<ele2<<","<<ele3<<")"<<" ";
+
+int main() {
+
+    int arr[] = {4,5,3,6,0,6};
+    int n = 6;
+    int target = 12;
+
+    for(int i = 0; i < n - 2; i++) {
+
+        for(int j = i + 1; j < n - 1; j++) {
+
+            for(int k = j + 1; k < n; k++) {
+
+                if(arr[i] + arr[j] + arr[k] == target) {
+
+                    cout << "("
+                         << arr[i] << ", "
+                         << arr[j] << ", "
+                         << arr[k] << ")" << endl;
+                }
             }
-          }
         }
-     }
     }
+
     return 0;
 }
 // OUTPUT-
