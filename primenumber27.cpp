4e05f19dc62c87b3b5bcf323fd2131f6e3425706
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-14 17:56:47 +0530

    Update primenumber27.cpp

--- primenumber27.cpp
+++ primenumber27.cpp
@@ -1,3 +1,9 @@
+// | `break`                               | `continue`                         |
+// | ------------------------------------- | ---------------------------------- |
+// | Ends the entire loop                  | Skips only the current iteration   |
+// | Control goes outside the loop         | Control goes to the next iteration |
+// | Remaining iterations are not executed | Remaining iterations still execute |
+
 #include<iostream>
 using namespace std;
 int main(){
