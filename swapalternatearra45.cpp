f6637530634c609184f27dc60a57a0aed35e5162
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 11:58:41 +0530

    Refactor swap function into Solution class
    Refactored the swap function to use a class and vector, replacing the original implementation with a method in the Solution class.

--- swapalternatearra45.cpp
+++ swapalternatearra45.cpp
@@ -1,33 +1,10 @@
-#include<iostream>
-using namespace std;
-void printarray (int arr[],int n){
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }cout<<endl;
-}
-
-void swapalternate(int arr[], int n){
-    for(int i=0;i<n;i=i+2){
-if(i+1<n){
-   
-   //  swap(arr[i],arr[i+1]);
-   int temp=arr[i];
-    arr[i]=arr[i+1];
-    arr[i+1]=temp;
-}
-}
-}
-int main(){
-    int arr[6]={1,2,7,8,5,9};
-    int brr[5]={1,2,3,4,5};
-    swapalternate(arr,6);
-    printarray(arr,6);
-    cout<<endl;
-    swapalternate(brr,5);
-    printarray(brr,5);
-    return 0;
-}
-// OUTPUT-
-// 2 1 8 7 9 5 
-
-// 2 1 4 3 5 
+class Solution {
+public:
+    void swapAlternate(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i += 2) {
+            int temp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = temp;
+        }
+    }
+};
