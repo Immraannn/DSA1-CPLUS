497217e61a7e60d1c1f2b201dbcc9754f6e05c3a
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 12:37:09 +0530

    Update binaryoccurence52.cpp

--- binaryoccurence52.cpp
+++ binaryoccurence52.cpp
@@ -1,56 +1,61 @@
-#include<iostream>
-using namespace std;
-int firstoccurence(int arr[],int n,int key){
-    int s=0;int e=n-1;
-  int  mid=s+(e-s)/2;
-  int ans=-1;
-  while(s<=e){
-if(arr[mid]==key){
-    ans=mid;
-    e=mid-1;
-}
-else if(key>arr[mid]){//right mn jao
-    s=mid+1;
-}
-else if(key<arr[mid]){//left mn jao
-e=mid-1;
-}
-mid=s+(e-s)/2;
-  }
-  return ans;
-}
+class Solution {
+public:
+    int firstOccurrence(vector<int>& nums, int target) {
 
-int lastoccurence(int arr[],int n,int key){
-    int s=0;int e=n-1;
-  int  mid=s+(e-s)/2;
-  int ans=-1;
-  while(s<=e){
-if(arr[mid]==key){
-    ans=mid;
-    s=mid+1;
-}
-else if(key>arr[mid]){//right mn jao
-    s=mid+1;
-}
-else if(key<arr[mid]){//left mn jao
-e=mid-1;
-}
-mid=s+(e-s)/2;
-  }
-  return ans;
-}
-int main(){
-    int even[5]={1,2,3,3,5};
-    int even1[11]={1,2,3,3,3,3,3,3,3,3,5};
-    cout<<" first occurencce of 3 at index "<<firstoccurence(even,5,3)<<endl;
-    cout<<" last occurencce of 3 at index "<<lastoccurence(even,5,3)<<endl;
-    cout<<" first occurencce of 3 at index "<<firstoccurence(even1,11,3)<<endl;
-    cout<<" last occurencce of 3 at index "<<lastoccurence(even1,11,3)<<endl;
-    return 0;
-    // if total occurence to find then (lastindex-firstindex)+1
-}
-// OUTPUT-
-// first occurencce of 3 at index 2
-//  last occurencce of 3 at index 3
-//first occurencce of 3 at index 2
-//last occurencce of 3 at index 9
+        int left = 0;
+        int right = nums.size() - 1;
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;          // Store answer
+                right = mid - 1;    // Continue searching left
+            }
+
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+
+class Solution {
+public:
+    int lastOccurrence(vector<int>& nums, int target) {
+
+        int left = 0;
+        int right = nums.size() - 1;
+        int ans = -1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;         // Store answer
+                left = mid + 1;    // Continue searching right
+            }
+
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+
+            else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+};
