69abfcebf9e27efcdc4c960ed13300893c7b7602
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 12:00:23 +0530

    Convert unique element function to class method
    Refactor unique element finder to use a class method.

--- uniquelementarr46.cpp
+++ uniquelementarr46.cpp
@@ -1,17 +1,15 @@
-#include<iostream>
-using namespace std;
-int unique(int arr[],int n){
-    int ans=0;
-    for(int i=0;i<n;i++){
-        ans=ans^arr[i];
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans ^= nums[i];
+        }
+
+        return ans;
     }
-    return ans;
-}
-int main(){
-    int arr[5]={1,3,4,1,3};
-    cout<<"unique element is "<<unique(arr,5);
-    return 0;
-}
+};
 // OUTPUT-
 // unique element is 4
 // 🔹 Key Idea: XOR Properties (Very Important)
