4c18c88b1c9596cbe32321ab46f4ca20c513efea
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 11:56:07 +0530

    Refactor linear search to use vector
    Updated linear search implementation to use vector instead of array. Improved code structure and readability.

--- linearsearcharr43.cpp
+++ linearsearcharr43.cpp
@@ -1,4 +1,40 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+// Returns the index of key if found, otherwise -1
+int linearSearch(vector<int>& arr, int key) {
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == key)
+            return i;
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {10, 20, 30, 40, 50};
+    int key = 30;
+
+    int index = linearSearch(arr, key);
+
+    if (index != -1)
+        cout << "Element found at index " << index;
+    else
+        cout << "Element not found";
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+#include <iostream>
 using namespace std;
 
 int linearSearch(int arr[], int n, int key) {
