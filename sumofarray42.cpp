4b34d07b21a66952abfebe85df5487d026a3d6c1
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 11:54:27 +0530

    Refactor sum calculation to use vector and findSum
    Refactored the sum calculation to use a vector and added a new findSum function. Updated main to demonstrate the new functionality with a predefined vector.

--- sumofarray42.cpp
+++ sumofarray42.cpp
@@ -1,3 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Function to calculate sum of elements
+int findSum(vector<int>& arr) {
+    int sum = 0;
+
+    for (int i = 0; i < arr.size(); i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int main() {
+    vector<int> arr = {10, 20, 30, 40, 50};
+
+    cout << "Sum = " << findSum(arr);
+
+    return 0;
+}
+
+
+
+
+
+
+
 #include<iostream>
 int sum(int arr[],int size){
     int sum=0;
