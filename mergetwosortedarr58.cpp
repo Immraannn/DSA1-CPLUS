c3bfcc18aecfed75feecaaf5c4bfdb93c895d8d1
Md Imran Shakeel <imshkl2005@gmail.com>
2026-07-15 13:19:09 +0530

    Update mergetwosortedarr58.cpp

--- mergetwosortedarr58.cpp
+++ mergetwosortedarr58.cpp
@@ -1,35 +1,88 @@
-#include<iostream>
-using namespace std;
+class Solution {
+public:
+    vector<int> mergeSortedArray(vector<int>& nums1, vector<int>& nums2) {
 
-int main(){
+        vector<int> ans;
 
-    int arr1[]={1,3,5};
-    int arr2[]={2,4,6};
+        int i = 0;
+        int j = 0;
 
-    int n1=3,n2=3;
+        while (i < nums1.size() && j < nums2.size()) {
 
-    int i=0,j=0;
+            if (nums1[i] < nums2[j]) {
+                ans.push_back(nums1[i]);
+                i++;
+            }
+            else {
+                ans.push_back(nums2[j]);
+                j++;
+            }
+        }
 
-    while(i<n1 && j<n2){
-
-        if(arr1[i]<arr2[j]){
-            cout<<arr1[i]<<" ";
+        // Remaining elements of nums1
+        while (i < nums1.size()) {
+            ans.push_back(nums1[i]);
             i++;
         }
 
-        else{
-            cout<<arr2[j]<<" ";
+        // Remaining elements of nums2
+        while (j < nums2.size()) {
+            ans.push_back(nums2[j]);
             j++;
         }
-    }
 
-    while(i<n1){
-        cout<<arr1[i]<<" ";
-        i++;
+        return ans;
     }
+};
 
-    while(j<n2){
-        cout<<arr2[j]<<" ";
-        j++;
+
+
+
+
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        // Pointer to the last valid element in nums1
+        int i = m - 1;
+
+        // Pointer to the last element in nums2
+        int j = n - 1;
+
+        // Pointer to the last position of nums1
+        // (where the largest element should be placed)
+        int k = m + n - 1;
+
+        // Compare elements from the end of both arrays
+        while (i >= 0 && j >= 0) {
+
+            // If current element in nums1 is larger,
+            // place it at position k
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            }
+
+            // Otherwise place element from nums2
+            else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+
+            // Move to the next position from the end
+            k--;
+        }
+
+        // If any elements are left in nums2,
+        // copy them into nums1
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+
+        // No need to copy remaining elements of nums1
+        // because they are already in their correct positions.
     }
-}
+};
