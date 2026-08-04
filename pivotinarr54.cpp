08afd4fcf483c0fffc849312ce931b21428a9daf
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-04 10:16:51 +0530

    Update pivotinarr54.cpp

--- pivotinarr54.cpp
+++ pivotinarr54.cpp
@@ -1,3 +1,11 @@
+// Why is it called Pivot?
+// Imagine the array as a circle.
+// Original
+// 1 2 3 4 5 6 7
+// Rotate
+// 5 6 7 1 2 3 4
+// Everything is still sorted except one "breaking point".
+// That breaking point is the pivot.
 class Solution {
 public:
     int findPivot(vector<int>& nums) {
