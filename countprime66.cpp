fc4f16aedf794b0a90153905d7a2e4d07cddf7fc
Md Imran Shakeel <imshkl2005@gmail.com>
2026-08-04 13:36:13 +0530

    Update countprime66.cpp

--- countprime66.cpp
+++ countprime66.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 using namespace std;
 
-bool isPrime(int num) {
-    if (num < 2)
+bool isPrime(int n) {
+    if (n < 2)
         return false;
 
-    if (num == 2)
-        return true;
-
-    if (num % 2 == 0)
-        return false;
-
-    for (int i = 3; i * i <= num; i += 2) {
-        if (num % i == 0)
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
             return false;
     }
 
